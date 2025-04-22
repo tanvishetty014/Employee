8eeb72455228da6b9c926e6d4407c1d36fbd0905
tanvishetty014 <tanvishetty407@gmail.com>
2025-04-22 15:22:53 +0530

    Message

---
+++ Dancer.java
@@ -0,0 +1,16 @@
+public class Dancer {
+    public static void main(String[] args) {
+        String name ="tanvi";
+        int id = 87;
+         String dept = "BCA";
+         int age = 20;
+ 
+         System.out.println("Employee Deails");
+         System.out.println("---------------");
+         System.out.println("Name: "+name);
+         System.out.println("Age: "+age);
+         System.out.println("Employee number: "+ id);
+         System.out.println("Department: "+dept);
+   }
+    
+}
---
+++ Student.java
@@ -0,0 +1,15 @@
+public class Student{
+  public static void main(String[] args) {
+       String name ="tanvi";
+       int srn = 87;
+        String dept = "BCA";
+        int age = 20;
+
+        System.out.println("Student Deails");
+        System.out.println("---------------");
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Student registration number: "+srn);
+        System.out.println("Department: "+dept);
+  }
+}
